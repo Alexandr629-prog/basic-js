438837e5afc62c7f51241f83195b5c728d7d3783
Alexandr629-prog <aaskaldovic@gmail.com>
2020-03-22 23:35:53 +0300

    start task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let k = 0;
+
+  matrix.forEach(array => {
+    array.forEach(elem => {
+      if (elem === '^^') {
+        k++;
+      }
+    });
+  });
+
+  return count;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+
+module.exports = function createDreamTeam(usernames) {
+  if (usernames === undefined || usernames === null) return false;
+  let result = [];
+  for (let i = 0; i < usernames.length; i++) {
+      if (typeof usernames[i] === "string") {
+          result.push(names[i].trim().charAt(0).toUpperCase());
+      }
+  }
+  if (result.length > 0) {
+      let rString = "";
+      result.sort();
+      for (let i = 0; i < result.length; i++) {
+          rString += result[i];
+      }
+      return rString;
+  } else return false;
 };
